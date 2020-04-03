8dda746429bdb916c340735f3edbfe7844bf9865
kangchao <kangchao@100tal.com>
2020-04-03 09:45:12 +0800

    P_2_0_01

--- P_2_0_01/main.js
+++ P_2_0_01/main.js
@@ -1,7 +1,7 @@
-let winH = 800;
-let winW = 800;
+let winH = 600;
+let winW = 600;
 let canvas = null;
-let max = 400;
+let max = 300;
 let colors = [];
 
 function setup() {
@@ -20,13 +20,16 @@ function draw() {
 	let sw = map(mouseX, 0, winW, 1, 10);
 	sw = parseInt(constrain(sw, 1, max));
 
+	let len = map(mouseX, 0, winW, -winW, winW);
+	len = Math.abs(len) / 2;
+
 	let degree = Math.PI * 2 / cnt;
 	translate(winW / 2, winH / 2);
 	strokeWeight(sw);
 	for (let i = 0; i < cnt; i++) {
 		stroke(colors[i]);
-		let x = Math.cos(i * degree) * winW;
-		let y = Math.sin(i * degree) * winW;
+		let x = Math.cos(i * degree) * len;
+		let y = Math.sin(i * degree) * len;
 		line(0, 0, x, y);
 	}
 }
